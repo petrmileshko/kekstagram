8faed422ecfa2827c3813b34b4d7552395588869
Peter <mileshko.petr@yandex.ru>
2023-09-01 17:30:11 +0300

    updated
    Вывод картинок на страницу

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {
-  createPosts
-} from './create-posts.js'; // В качестве параметра принимает значение - количество постов, которое необходимо сгенерировать
+  picturesContainer
+} from './render-pictures.js'; // В качестве параметра принимает значение - количество постов, которое необходимо сгенерировать
 
-console.log(createPosts(5));
+
---
+++ js/render-pictures.js
@@ -0,0 +1,28 @@
+import {
+  createPosts
+} from './create-posts.js'; // В качестве параметра принимает значение - количество постов, которое необходимо сгенерировать
+
+const picturesContainer = document.querySelector('.pictures');
+const picturesAll = createPosts(25);
+
+const templatePictureItem = document.querySelector('#picture').content.querySelector('.picture');
+
+const templateContainer = document.createDocumentFragment();
+
+picturesAll.forEach(({
+  url,
+  likes,
+  comments
+}) => {
+  const newPicture = templatePictureItem.cloneNode(true);
+  newPicture.querySelector('.picture__img').src = url;
+  newPicture.querySelector('.picture__comments').textContent = comments.length;
+  newPicture.querySelector('.picture__likes').textContent = likes;
+  templateContainer.append(newPicture);
+});
+
+picturesContainer.append(templateContainer);
+
+export {
+  picturesContainer
+};
