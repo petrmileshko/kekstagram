283c72f5e20195801479f9de732a3f4a293ba227
Peter <mileshko.petr@yandex.ru>
2023-09-04 20:11:10 +0300

    Update upload-picture.js
    Добавляем обработчик нажатия на клавиши для полей ввода в фокусе

--- js/upload-picture.js
+++ js/upload-picture.js
@@ -1,12 +1,17 @@
 import {
   isEscape,
   isEnter
-} from './utils.js'; // Один параметр с кодом клавиши из свойтсва объекта Event.key
+} from './utils.js'; // Один параметр с кодом клавиши из свойства объекта Event.key
 
 const modal = document.querySelector('.img-upload__overlay');
 const closeButton = modal.querySelector('#upload-cancel');
 const uploadElement = document.querySelector('#upload-file');
+const hashtags = document.querySelector('[name="hashtags"]');
+const description = document.querySelector('[name="description"]');
 
+/**
+  добавляем callback функции для обработчиков
+ */
 function onUploadFileClick(e) {
   e.preventDefault();
   openModal();
@@ -31,25 +36,51 @@ function onCloseButtonEnterDown(e) {
   }
 }
 
-function openModal() {
-  modal.classList.remove('hidden');
-  document.
-    addEventListener('keydown', onPopUpEscDown);
-  closeButton.
-    addEventListener('click', onCloseButtonClick);
-  closeButton.
-    addEventListener('keydown', onCloseButtonEnterDown);
+function onHashtagsEscDown(e) {
+  if (isEscape(e.key)) {
+    e.stopPropagation();
+  }
 }
 
-function closeModal() {
-  modal.classList.add('hidden');
-  uploadElement.value = '';
+function onDescriptionEscDown(e) {
+  if (isEscape(e.key)) {
+    e.stopPropagation();
+  }
+}
+
+//Функция для открытия модального окна
+function openModal() {
+  modal.classList.remove('hidden');                            //Показываем модальное окно с формой
+  document.body.classList.add('modal-open');                   //Убираем прокрутку основного экрана
   document.
-    removeEventListener('keydown', onPopUpEscDown);
+    addEventListener('keydown', onPopUpEscDown);               //Добавляем на страницу обработчик нажатия клавиши Esc по которому будет закрываться модальное окно
+  closeButton.
+    addEventListener('click', onCloseButtonClick);             //Добавляем на кнопку с крестиком обработчик клика мышки, для закрытия модального окна с формой
+  closeButton.
+    addEventListener('keydown', onCloseButtonEnterDown);       //Добавляем на кнопку с крестиком обработчик нажатия клавиши Enter, для закрытия модального окна с формой
+  hashtags.
+    addEventListener('keydown', onHashtagsEscDown);            //Отменяем на поле ввода хештегов, закрытие окна по нажатию клавиши Esc
+  description.
+    addEventListener('keydown', onDescriptionEscDown);         //Отменяем на поле ввода описания, закрытие окна по нажатию клавиши Esc
+}
+
+//Функция для закрытия модального окна
+function closeModal() {
+  modal.classList.add('hidden');                                 //Прячем модальное окно с формой
+  uploadElement.value = '';                                      //Обнуляем поля формы
+  hashtags.value = '';
+  description.value = '';
+  document.body.classList.remove('modal-open');                  //Восстанавливаем прокрутку основного экрана
+  document.
+    removeEventListener('keydown', onPopUpEscDown);              //Удаляем не нужные обработчики событий в модальном окне
   closeButton.
     removeEventListener('click', onCloseButtonClick);
   closeButton.
     removeEventListener('keydown', onCloseButtonEnterDown);
+  hashtags.
+    removeEventListener('keydown', onHashtagsEscDown);
+  description.
+    removeEventListener('keydown', onDescriptionEscDown);
 }
 
-uploadElement.addEventListener('change', onUploadFileClick);
+uploadElement.addEventListener('change', onUploadFileClick);     //На поле загрузки файла вешаем обработчик события выбора файла
